1fffd320fb2757422ed39a4218d856b1eae814e3
Chijioke Ugwuanyi <chijioke@Chijiokes-MBP.lan>
2021-04-24 10:27:56 +0100

    fixed up product model.

--- app/models/ProductModel.js
+++ app/models/ProductModel.js
@@ -1,16 +1,11 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var ItemSchema = new Schema({
-	'addedby' : {
-	 	type: Schema.Types.ObjectId,
-	 	ref: 'User'
-	},
-	'item' : String,
-    'name' : String,
+var ProductSchema = new Schema({
+	'name' : String,
+    'quantity' : Number,
 	'price' : Number,
-    'status' : {type: String, default: "PENDING"},
 	'createdon' : {type: Date, default: Date.now()}
 });
 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model('Product', ProductSchema);
